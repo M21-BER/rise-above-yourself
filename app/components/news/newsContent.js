b28072e9884d9bf79943ed8179ee7971f36e369b
Musse Berhane <musseberhanf7@gmail.com>
2025-07-09 23:33:22 +0300

    changed nes content 2

--- app/components/news/newsContent.js
+++ app/components/news/newsContent.js
@@ -3,7 +3,7 @@ export const newsContent = [
     id: 2,
     title: "Ray Athletics Launches with a New Vision for Fitness",
     excerpt:
-      "R.A.Y. Athletics is proud to announce the opening of its new performance gym in Virginia. The space is now officially open and welcoming youth and adult athletes for structured, intentional training rooted in discipline, soccer, and community. Founded by athlete and coach Rediet Berihun, R.A.Y. Athletics offers a unique training model that blends indoor gym work with outdoor field development. Built with a focus on both personal growth and athletic performance, the gym provides a home for athletes at every stage. Programs include functional group training for adults, one-on-one personal coaching, youth soccer development, and field and gym integration for total-body performance.",
+      "R.A.Y. Athletics is proud to announce the opening of its new performance gym in Virginia. The space is now officially open and welcoming youth and adult athletes...",
     image: "/images/news.png",
     date: "2025-07-09",
   },
